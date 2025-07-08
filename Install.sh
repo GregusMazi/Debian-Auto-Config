324ba29512f89edb69fcc2dcbb8e896dca2c9d6f
GREGUS MAZI <grzehoo78@hotmail.com>
2025-07-08 23:46:18 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://github.com/projectzerodays/Debian-Auto-Config
 cd Debian-Auto-Config
-chmod x+ Debian-Auto-Config.sh
+chmod +x Debian-Auto-Config.sh
 bash Debian-Auto-Config.sh
 cp ip /bin
